cd6b3f2b4998d5410efee6f4a20d1aec25c4b7dc
Amandeep Chawla <adchawla@gmail.com>
2016-07-21 03:47:26 +0530

    Logic to switch uikit implemented

--- AbstractFactory/AbstractFactory/main.cpp
+++ AbstractFactory/AbstractFactory/main.cpp
@@ -1,17 +1,47 @@
 #include "GTKApplicationWindow.h"
 #include "GTKStatusBarWindow.h"
 #include "GTKToolboxWindow.h"
+#include "QTApplicationWindow.h"
+#include "QTStatusBarWindow.h"
+#include "QTToolboxWindow.h"
+
 #include "GTKButton.h"
+#include <iostream>
 
 void setupToolbox( IToolboxWindow * toolboxWindow );
 void setupShapeButtons( IToolboxWindow * toolboxWindow );
 void setupPenButtons( IToolboxWindow * toolboxWindow );
 void setupEraserButton( IToolboxWindow * toolboxWindow );
 
-int main() {
-	IApplicationWindow * appWindow = new GTKApplicationWindow( 300, 400 );
-	IToolboxWindow * toolboxWindow = new GTKToolboxWindow( 50, 400 );
-	IStatusBarWindow * statusboxWindow = new GTKStatusBarWindow( 50, 400 );
+// global variable to keep track which UI toolkit is selected.
+// Note: not the best way to achieve the switch but minimize the effort a lot.
+int uikit = -1;
+
+int main(int argc, char ** argv) {
+
+	// logic to switch ui kit
+	uikit = 1;
+	if ( argc > 1 ) {
+		if ( strcmp( argv[ 1 ], "qt" ) == 0 ) {
+			uikit = 2;
+		}
+	}
+	
+	IApplicationWindow * appWindow = nullptr;
+	IToolboxWindow * toolboxWindow = nullptr;
+	IStatusBarWindow * statusboxWindow = nullptr;
+	switch ( uikit ) {
+	case 1:
+		appWindow = new GTKApplicationWindow( 300, 400 );
+		toolboxWindow = new GTKToolboxWindow( 50, 400 );
+		statusboxWindow = new GTKStatusBarWindow( 50, 400 );
+		break;
+
+	case 2:
+		appWindow = new QTApplicationWindow( 300, 400 );
+		toolboxWindow = new QTToolboxWindow( 50, 400 );
+		statusboxWindow = new QTStatusBarWindow( 50, 400 );
+	}
 	appWindow->setStatusBarWindow( statusboxWindow );
 	appWindow->setToolboxWindow( toolboxWindow );
 	statusboxWindow->setMessage( "CAPS: ON" );
